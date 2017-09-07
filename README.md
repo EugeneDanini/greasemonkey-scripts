a1ab0aa609137ddd8d2eec3e17ffc8387eec6916
E. Danini <e.danini@semrush.com>
2017-09-07 16:23:02 +0300

    added scripts and readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# greasemonkey-scripts
+# Scripts for greasemonkey firefox add-on
+
+## Usage
+Clone repository into directory gm_scripts in your firefox profile.
+For example:
+```bash
+cd ~/.mozilla/firefox/4tthpaav.default/gm_scripts
+git clone https://github.com/EvgenyDanini/greasemonkey-scripts .
+```
+
+### disable_lj_ads
+Sets livejournal.com advertisement and promotions to display none
+
+### disable_vk_ads
+Sets vk.com advertisement and promotions to display none
---
+++ disable_lj_ads/disable_lj_ads.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        disable lj ads
+// @namespace   lj
+// @include       http://livejournal.com/*
+// @include       https://livejournal.com/*
+// @include       http://*.livejournal.com/*
+// @include       https://*.livejournal.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+(function() {
+var css = ".allbanners, .b-discoverytimes-wrapper, .entryunit--feedpromo-entry, .recommended-entries { display:none !important }";
+
+var heads = document.getElementsByTagName("head");
+if (heads.length > 0) {
+	var node = document.createElement("style");
+	node.type = "text/css";
+	node.appendChild(document.createTextNode(css));
+	heads[0].appendChild(node);
+}
+})();
